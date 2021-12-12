5be71795f816b5383702e069f0f3822d7f8b6ebb
asimzohairyazdani <85032912+asimzohairyazdani@users.noreply.github.com>
2021-12-12 23:49:17 +0530

    Create rev.py

---
+++ rev.py
@@ -0,0 +1,9 @@
+word = input("Input a word to reverse: ")
+
+for char in range(len(word) - 1, -1, -1):
+  print(word[char], end="")
+print("\n")
+
+output:
+  Input a word to reverse: asim
+    misa
