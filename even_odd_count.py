894f27e9f2469ae524e5a6e2684d6bfcd3d800cb
asimzohairyazdani <85032912+asimzohairyazdani@users.noreply.github.com>
2021-12-12 23:55:44 +0530

    Create even_odd_count.py

---
+++ even_odd_count.py
@@ -0,0 +1,17 @@
+numbers = [10, 21, 4, 45, 66, 93, 1]
+  
+even_count, odd_count = 0, 0
+for num in numbers:
+    
+    if num % 2 == 0:
+        even_count += 1
+  
+    else:
+        odd_count += 1
+          
+print("Even numbers in the list: ", even_count)
+print("Odd numbers in the list: ", odd_count)
+
+Output:
+Even numbers in the list:  3
+Odd numbers in the list:  4
